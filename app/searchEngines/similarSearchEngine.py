ae04de0bd507086311ffe8ca861390339414cc31
shizzz <kolmakov7@gmail.com>
2026-02-10 20:26:40 +0700

    fix when similars found two books with same name

--- app/searchEngines/similarSearchEngine.py
+++ app/searchEngines/similarSearchEngine.py
@@ -4,6 +4,7 @@ from app.models import Book, Embedding, Feedback
 class SimilarSearchEngine:
     def __init__(self, exclude_same_authors: bool):
         self._exclude_same_authors = exclude_same_authors
+        self._seen_titles = set()
 
     def _should_skip(self, source: Book, candidate_name: str, candidate_title: str) -> bool:
         if source.file_name is not None and source.file_name == candidate_name:
@@ -12,9 +13,14 @@ class SimilarSearchEngine:
         if source.title is not None and source.title == candidate_title:
             return True
 
+        if candidate_title in self._seen_titles:
+            return True
+
         # if self.exclude_same_authors and source.authors and candidate.authors:
         #     if set(source.authors) & set(candidate.authors):
         #         return True
+        
+        self._seen_titles.add(candidate_title)
 
         return False
 
