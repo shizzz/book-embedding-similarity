de868ce964acefe36ef8147afe83bdc701453f41
Kolmakov Sergey <kolmakov7@gmail.com>
2026-03-12 18:10:57 +0700

    rollback index command

--- app/cli/index/generate.py
+++ app/cli/index/generate.py
@@ -1,7 +1,19 @@
-
-import asyncio
-from app.workers import GenerateIndexWorker
+from app.ui import LiveUI
+from app.hnsw.services import BookEmbeddingIndexer
+from app.infrastructure.db.router import DBRouter
+from app.workers.stats import NullStats
 
 def run(args):
-    worker = GenerateIndexWorker()
-    asyncio.run(worker.run())
+    router = DBRouter()
+    ui = LiveUI(
+        stats=NullStats(),
+        max_workers = 0,
+        title = "Create HNSW index",
+        show_table = False
+    )
+    ui.init()
+
+    BookEmbeddingIndexer(
+        db_router=router,
+        ui=ui
+    ).build_index()
