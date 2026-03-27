6c8051d836bf5a482567e28a57593827d7ce1299
Kolmakov Sergey <kolmakov7@gmail.com>
2026-03-27 14:19:19 +0700

    clean ui

--- app/ui/live_ui.py
+++ app/ui/live_ui.py
@@ -8,16 +8,10 @@ from rich.text import Text
 from rich.progress_bar import ProgressBar
 from rich.progress import (
     Progress,
-    BarColumn,
-    TextColumn,
-    TimeRemainingColumn,
-    TimeElapsedColumn,
-    TaskProgressColumn,
     TaskID
 )
 from time import perf_counter
 from .ui import BaseUI
-from .tqdmLike import TqdmLike, TqdmIterable
 from app.workers.stats import Stats
 from app.infrastructure.models import Report
 
@@ -47,6 +41,10 @@ class LiveUI(BaseUI):
         async with self.lock:
             self.live.update(self.layout())
 
+    def init(self):
+        self.live = Live(self.layout(), refresh_per_second=1, console=self.console)
+        self.live.start()
+        
     def _make_stats_table(self) -> Table:
         table = Table(expand=True)
 
@@ -166,153 +164,6 @@ class LiveUI(BaseUI):
 
         return table
 
-    def _compute_speed(self, idx: int, count: int) -> str:
-        now = perf_counter()
-
-        if idx not in self._speed_history:
-            self._speed_history[idx] = {
-                "last_time": now,
-                "ema_speed": 0.0
-            }
-            return "0"
-
-        data = self._speed_history[idx]
-
-        dt = now - data["last_time"]
-        data["last_time"] = now
-
-        if dt <= 0:
-            return "0"
-
-        instant_speed = count / dt
-
-        alpha = 0.2  # чем меньше — тем плавнее
-
-        if data["ema_speed"] == 0:
-            data["ema_speed"] = instant_speed
-        else:
-            data["ema_speed"] = (
-                alpha * instant_speed +
-                (1 - alpha) * data["ema_speed"]
-            )
-
-        speed = data["ema_speed"]
-
-        if speed >= 10:
-            speed_fmt = f"{speed:.0f}"
-        elif speed >= 1:
-            speed_fmt = f"{speed:.1f}"
-        else:
-            speed_fmt = f"{speed:.2f}"
-
-        return speed_fmt
-        
-    def layout(self) -> Table:
-        grid = Table.grid(expand=True)
-        try:
-            if self._show_table:
-                grid.add_row(self._make_stats_table())
-                grid.add_row(self._make_edges_table())
-                if self.model_info:
-                    grid.add_row(self._make_model_info())
-                #grid.add_row(self._make_info())
-
-            for idx in self._bars:
-                grid.add_row(self._bars[idx])
-        except Exception as e:
-            print(e)
-        return grid
-    
-    def init(self):
-        self.live = Live(self.layout(), refresh_per_second=1, console=self.console)
-        self.live.start()
-
-    async def set_thread(self, worker_id: int, name: str):
-        self.live.update(self.layout())
-
-    async def done_async(self, idx: int = 0, count: int = 1):
-        speed = self._compute_speed(idx, count)
-        async with self.lock:
-            self._bars[idx].update(
-                task_id=self._tasks[idx],
-                advance=count,
-                custom_speed=speed
-            )
-            self.live.update(self.layout())
-
-    def done(self, idx: int = 0, count: int = 1):
-        speed = self._compute_speed(idx, count)
-        self._bars[idx].update(
-            task_id=self._tasks[idx], 
-            advance=count,
-            custom_speed=speed
-        )
-        self.live.update(self.layout())
-
-    async def update_total_async(self, total: int, idx: int = 0):
-        async with self.lock:
-            self._bars[idx].update(self._tasks[idx], total=total)
-
-    def update_total(self, total: int, idx: int = 0):
-        self._bars[idx].update(self._tasks[idx], total=total)
-
-    async def error(self, idx: int = 0):
-        self._bars[idx].update(self._tasks[idx], advance=1)
-        self.live.update(self.layout())
-
-    def add_progress(
-            self,
-            descr: str, 
-            unit: str,
-            show_elapsed: bool = False
-        ) -> int:
-        idx = max(self._bars.keys(), default=-1) + 1
-
-        fixed_len = 25
-        descr_fixed = descr.ljust(fixed_len)[:fixed_len]
-
-        columns = [
-            TextColumn("[bold cyan]{task.description:>20}", justify="left"),
-            BarColumn(bar_width=40),
-            TextColumn("{task.completed:>6}/{task.total:<6}", justify="right"),
-            TaskProgressColumn(),
-            TextColumn(
-                "{task.fields[custom_speed]} {task.fields[unit]}/с",
-                justify="right",
-            ),
-            TimeRemainingColumn(),
-        ]
-        if show_elapsed:
-            columns.append(TimeElapsedColumn())
-
-        self._bars[idx] = Progress(*columns)
-
-        self._tasks[idx] = self._bars[idx].add_task(
-            description=f"[bold green]{descr_fixed}",
-            total=0,
-            unit=unit,
-            custom_speed=0
-        )
-
-        return idx
-    
-    def remove_progress(self, idx: int) -> None:
-        if idx not in self._bars:
-            return
-
-        bar = self._bars[idx]
-
-        try:
-            bar.stop()
-        except Exception:
-            pass
-
-        del self._bars[idx]
-
-        if idx in self._tasks:
-            del self._tasks[idx]
-        self.live.update(self.layout())
-
     def _make_model_info(self):
         left = Table.grid(padding=(0, 1))
         left.add_column(style="cyan")
@@ -397,11 +248,22 @@ class LiveUI(BaseUI):
 
         return Panel(grid, title="Embedding Model", border_style="blue")
 
-    def tqdm(self, obj=None, desc: str = "", total: int = 0, unit: str = "", show_elapsed: bool = False):
-        if hasattr(obj, "__iter__") and obj is not None:
-            return TqdmIterable(self, obj, desc, unit, show_elapsed)
-        return TqdmLike(self, desc, total, unit, show_elapsed)
-    
+    def layout(self) -> Table:
+        grid = Table.grid(expand=True)
+        try:
+            if self._show_table:
+                grid.add_row(self._make_stats_table())
+                grid.add_row(self._make_edges_table())
+                if self.model_info:
+                    grid.add_row(self._make_model_info())
+                #grid.add_row(self._make_info())
+
+            for idx in self._bars:
+                grid.add_row(self._bars[idx])
+        except Exception as e:
+            print(e)
+        return grid
+
     def report(self, report: Report):
         """Красивый компактный отчет всех блоков"""
         grid = Table.grid(expand=True)
--- app/ui/ui.py
+++ app/ui/ui.py
@@ -1,32 +1,5 @@
 from abc import ABC
 
 class BaseUI(ABC):
-    def add_progress(self, descr: str, unit: str, show_elapsed: bool = False) -> int:
-        pass
-
-    def remove_progress(self, idx: int) -> None:
-        pass
-
-    def update_total(self, total: int, idx: int = 0):
-        pass
-
-    def done(self, idx: int = 0, count: int = 1):
-        pass
-
     def init(self):
-        pass
-
-    async def done_async(self, idx: int = 0, count: int = 1):
-        pass
-
-    async def update_total_async(self, total: int, idx: int = 0):
-        pass
-
-    async def set_thread(self, worker_id: int, name: str):
-        pass
-
-    async def error(self, idx: int = 0):
-        pass
-
-    def tqdm(self, desc: str, total: int = 0, unit: str = "", show_elapsed: bool = False):
         pass
